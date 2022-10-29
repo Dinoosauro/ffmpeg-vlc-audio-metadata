cc81270bdafc078485347f89c9494bc695796fa2
Dinoosauro <80783030+Dinoosauro@users.noreply.github.com>
2022-10-29 22:13:37 +0200

    Bug fixes and improvements
    Switched to switch instead of if/else statements
    Pass file name from the startup arguments (this means that you can use it, for example, in scripts).
    Bug fixes

--- Program.cs
+++ Program.cs
@@ -8,47 +8,52 @@ internal class Program
     static string extension = "ogg";
     static string beforeFfmpeg = "";
     static string additionalinfo = " -map_metadata -1 -vn";
+    static string getCommandType = "cmd";
+    static string commandIntro = "/c ";
 
 
     private static void Main(string[] args)
     {
+        var autoConvert = false;
+        var newFilePath = "";
         for (int i = 0; i < args.Length; i++)
         {
-            if (args[i].Contains("--codec"))
+            switch (args[i])
             {
-                library = args[i + 1];
+                case "--codec":
+                    library = args[i + 1];
+                    break;
+                case "--bitrate":
+                    bitrate = args[i + 1];
+                    break;
+                case "--extension":
+                    extension = args[i + 1];
+                    break;
+                case "--additional":
+                    additionalinfo = additionalinfo + " " + args[i + 1];
+                    break;
+                case "--additionalcustom":
+                    additionalinfo = " " + args[i + 1];
+                    break;
+                case "--printversion":
+                    AnsiConsole.WriteLine("ffmpeg-audio-metadata | 1.0.1");
+                    return;
+                case "--opensource":
+                    AnsiConsole.WriteLine("This application uses third-party libraries. You can see the licenses there: https://github.com/Dinoosauro/ffmpeg-audio-metadata/blob/main/OpenSourceLicenses.md");
+                    return;
+                case "--license":
+                    AnsiConsole.WriteLine("You can find a copy of ffmpeg-audio-metadata's license here: https://github.com/Dinoosauro/ffmpeg-audio-metadata/blob/main/LICENSE");
+                    return;
+                case "--fromfile":
+                    autoConvert = true;
+                    newFilePath = args[i + 1];
+                    break;
+                case "--everything":
+                    autoConvert = true;
+                    newFilePath = "--everything " + args[i + 1];
+                    break;
             }
-            if (args[i].Contains("--bitrate"))
-            {
-                bitrate = args[i + 1];
-            }
-            if (args[i].Contains("--extension"))
-            {
-                extension = args[i + 1];
-            }
-            if (args[i].Contains("--additional"))
-            {
-                additionalinfo = additionalinfo + " " + args[i + 1];
-            }
-            if (args[i].Contains("--additionalcustom"))
-            {
-                additionalinfo = " " + args[i + 1];
-            }
-            if (args[i].Contains("--printversion"))
-            {
-                AnsiConsole.WriteLine("FfmpegAudioMetadata | 1.0.0");
-                return;
-            }
-            if (args[i].Contains("--opensource"))
-            {
-                AnsiConsole.WriteLine("This application uses third-party libraries. You can see the licenses there: https://github.com/Dinoosauro/FfmpegAudioMetadata/blob/main/OpenSourceLicenses.md");
-                return;
-            }
-            if (args[i].Contains("--license"))
-            {
-                AnsiConsole.WriteLine("You can find a copy of FfmpegAudioMetadata's license here: https://github.com/Dinoosauro/FfmpegAudioMetadata/blob/main/LICENSE");
-                return;
-            }
+
         }
         if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
@@ -58,18 +63,30 @@ internal class Program
                 beforeFfmpeg = "./";
             }
         }
+        if (autoConvert)
+        {
+            check(newFilePath);
+            return;
+        }
+
         applicationGo();
     }
     static void applicationGo()
     {
         AnsiConsole.Write(
-    new FigletText("FfmpegAudioMetadata")
+    new FigletText("ffmpeg-audio-metadata")
         .LeftAligned()
         .Color(Spectre.Console.Color.White));
-        Console.WriteLine("Write the file you want to convert using " + library + ". If you want to convert every item of a folder, write --everything *fileextension*.");
-        var file = Console.ReadLine();
-        var getCommandType = "cmd";
-        var commandIntro = "/c ";
+        var file = AnsiConsole.Ask<string>("Write the file you want to convert using " + library + ". If you want to convert every item of a folder, write --everything *fileextension*.");
+        check(file);
+        AnsiConsole.MarkupLine("[bold green]Conversion ended.[/]");
+        if (AnsiConsole.Confirm("Do you want to convert another file?"))
+        {
+            applicationGo();
+        }
+    }
+    static void check(string file)
+    {
         if (!RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
         {
             getCommandType = "/bin/bash";
@@ -94,49 +111,48 @@ internal class Program
                 }
             }
         }
-        AnsiConsole.MarkupLine("[bold green]Conversion ended.[/]");
-        if (AnsiConsole.Confirm("Do you want to convert another file?"))
-        {
-            applicationGo();
-        }
-
-        void convert(string getUserFile)
-        {
-            AnsiConsole.Status()
-    .Start("Preparing for conversion...", ctx =>
-    {
-        ctx.Status("Getting file tag...");
-        TagLib.File tag = TagLib.File.Create(getUserFile);
-        if (bitrate == "tobedecided")
-        {
-            bitrate = Convert.ToString(tag.Properties.AudioBitrate);
-        }
-        if (extension == "m4a" || extension == "mp4" || extension == "aac")
-        {
-            additionalinfo = " -vcodec copy";
-        }
-        var process = "";
-        if (!RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
-        {
-
-            process = commandIntro + beforeFfmpeg + "ffmpeg -y -hide_banner -i '" + getUserFile + "' -acodec " + library + additionalinfo + " -b:a " + bitrate + "k '" + getUserFile.Substring(0, getUserFile.LastIndexOf(".")) + "." + extension + "'\"";
-        }
-        else
-        {
-            process = commandIntro + beforeFfmpeg + "ffmpeg -y -hide_banner -i \"" + getUserFile + "\" -acodec " + library + additionalinfo + " -b:a " + bitrate + "k \"" + getUserFile.Substring(0, getUserFile.LastIndexOf(".")) + "." + extension + "\"";
-        }
-        var fileName = getUserFile.Substring(0, getUserFile.LastIndexOf(".")).Replace("[", "(").Replace("]", ")");
-        if (fileName.Contains(Path.DirectorySeparatorChar))
-        {
-            fileName = fileName.Substring(fileName.LastIndexOf(Path.DirectorySeparatorChar) + 1);
-        }
-        ctx.Status("Encoding " + fileName + "...");
-        System.Diagnostics.Process.Start(getCommandType, process).WaitForExit();
-        TagLib.File newFile = TagLib.File.Create(getUserFile.Substring(0, getUserFile.LastIndexOf(".")) + ".ogg");
-        tag.Tag.CopyTo(newFile.Tag, true);
-        newFile.Save();
-    });
-        }
-
     }
+
+    static void convert(string getUserFile)
+    {
+        AnsiConsole.Status()
+.Start("Preparing for conversion...", ctx =>
+{
+    ctx.Status("Getting file tag...");
+    TagLib.File tag = TagLib.File.Create(getUserFile);
+    if (bitrate == "tobedecided")
+    {
+        bitrate = Convert.ToString(tag.Properties.AudioBitrate);
+    }
+    if (extension == "m4a" || extension == "mp4" || extension == "aac")
+    {
+        additionalinfo = " -vcodec copy";
+    }
+    var process = "";
+    if (bitrate.ToLower().Contains("k"))
+    {
+        bitrate = bitrate.ToLower().Substring(0, bitrate.ToLower().IndexOf("k"));
+    }
+    if (!RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows))
+    {
+
+        process = commandIntro + beforeFfmpeg + "ffmpeg -y -hide_banner -i '" + getUserFile + "' -acodec " + library + additionalinfo + " -b:a " + bitrate + "k '" + getUserFile.Substring(0, getUserFile.LastIndexOf(".")) + "." + extension + "'\"";
+    }
+    else
+    {
+        process = commandIntro + beforeFfmpeg + "ffmpeg -y -hide_banner -i \"" + getUserFile + "\" -acodec " + library + additionalinfo + " -b:a " + bitrate + "k \"" + getUserFile.Substring(0, getUserFile.LastIndexOf(".")) + "." + extension + "\"";
+    }
+    var fileName = getUserFile.Substring(0, getUserFile.LastIndexOf(".")).Replace("[", "(").Replace("]", ")");
+    if (fileName.Contains(Path.DirectorySeparatorChar))
+    {
+        fileName = fileName.Substring(fileName.LastIndexOf(Path.DirectorySeparatorChar) + 1);
+    }
+    ctx.Status("Encoding " + fileName + "...");
+    System.Diagnostics.Process.Start(getCommandType, process).WaitForExit();
+    TagLib.File newFile = TagLib.File.Create(getUserFile.Substring(0, getUserFile.LastIndexOf(".")) + "." + extension);
+    tag.Tag.CopyTo(newFile.Tag, true);
+    newFile.Save();
+});
+    }
+
 }
